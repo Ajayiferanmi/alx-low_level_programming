3100df09dd9bfc3b1ce700337a8815b88265fc8c
Ajayiferanmi <ajayiferanmi22@gmail.com>
2022-10-17 19:26:20 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio>
+/**
+ * main - entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'd';
+	while
+		(c <= 'z') {
+			putchar (c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar (c);
+			d++
+		}
+	return (0);
+}
