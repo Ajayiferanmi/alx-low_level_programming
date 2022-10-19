8458fce9d236ccec43444519e6ad7678987ed88e
Ajayiferanmi <ajayiferanmi22@gmail.com>
2022-10-19 05:42:39 -0700

    task 1 update 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print all alphabet lowercase
+ * print_alphabet - print all alphabet lowercase
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
