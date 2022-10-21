a8d491b74e803264093ee129eac3517cec2a5864
Ajayiferanmi <ajayiferanmi22@gmail.com>
2022-10-21 06:45:59 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
  * main - print fizzbuzz
  * Return: always 0
  */
-int main(void);
+int main(void)
 {
 	int n;
 
@@ -20,4 +20,5 @@ int main(void);
 			printf("%d", n);
 	}
 	printf("\n");
+	return (0);
 }
