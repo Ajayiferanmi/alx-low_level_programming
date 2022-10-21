93869df5f567941ebe7747e2500fd2aa2556197f
Ajayiferanmi <ajayiferanmi22@gmail.com>
2022-10-21 07:21:09 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,24 +2,21 @@
 
 /**
  * main - print fizzbuzz
- * Return: always 0
- */
+ * Return: Always 0
+ */'
 int main(void)
 {
-
 	int n;
 
 	printf("1");
 	for (n = 2; n <= 100; n++)
 	{
-
 		printf(" ");
 		if (n % 3 == 0)
 			printf("fizz");
 		if (n % 5 == 0)
 			printf("buzz");
-		if (n % 3 != 0 && n % 5 != 0)
-			printf("%d", n);
+		printf("%d", n);
 	}
 	printf("\n");
 	return (0);
